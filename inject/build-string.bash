f7bf4a0672d405530ddca1f6813c0b71c99f9ac0
Saul Shanabrook <s.shanabrook@gmail.com>
2016-07-15 18:44:39 -0400

    Fix building string to work with babel

--- inject/build-string.bash
+++ inject/build-string.bash
@@ -4,6 +4,6 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-printf "export default '" > dist/string.ts
-sed  "s/'/\\\'/g" dist/index.js |  perl -p -e "s/\n/\\\n/" >> dist/string.ts
-echo \' >> dist/string.ts
+printf "export default \`" > dist/string.ts
+sed  "s/\`/\\\\\`/g" dist/index.js >> dist/string.ts
+echo \` >> dist/string.ts
