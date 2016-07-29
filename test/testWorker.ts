f64756a0dae2722aba02948ff8bb0a3cec853f70
Saul Shanabrook <s.shanabrook@gmail.com>
2016-07-29 17:45:29 -0400

    add rsa-oaep tests

--- test/testWorker.ts
+++ test/testWorker.ts
@@ -43,6 +43,110 @@ test("getRandomValues can re updated", async function (t) {
   t.end();
 });
 
+// below copied from https://github.com/diafygi/webcrypto-examples
+
+test("RSA-OAEP - generateKey", async (t) => {
+  const key = await crypto.subtle.generateKey(
+    ({
+      name: "RSA-OAEP",
+      modulusLength: 2048, // can be 1024, 2048, or 4096
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: {name: "SHA-256"}, // can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+    } as Algorithm),
+    true, //  whether the key is extractable (i.e. can be used in exportKey)
+    ["encrypt", "decrypt"] //  can be any combination of "sign" and "verify"
+  );
+  t.true(key);
+  t.true(key.publicKey);
+  t.true(key.privateKey);
+  t.end();
+});
+
+test("RSA-OAEP - importKey", async (t) => {
+  const publicKey = await crypto.subtle.importKey(
+  "jwk", // can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+  ({   // this is an example jwk key, other key types are Uint8Array objects
+    kty: "RSA",
+    e: "AQAB",
+    n: "vGO3eU16ag9zRkJ4AK8ZUZrjbtp5xWK0LyFMNT8933evJoHeczexMUzSiXaLrEFSyQZortk81zJH3y41MBO_UFDO_X0crAquNrkjZDrf9Scc5-MdxlWU2Jl7Gc4Z18AC9aNibWVmXhgvHYkEoFdLCFG-2Sq-qIyW4KFkjan05IE",
+    alg: "RSA-OAEP-256",
+    ext: true,
+  } as any),
+  ({   // these are the algorithm options
+    name: "RSA-OAEP",
+    hash: {name: "SHA-256"}, // can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+  } as Algorithm),
+  true, // whether the key is extractable (i.e. can be used in exportKey)
+  ["encrypt"] // "encrypt" or "wrapKey" for public key import or
+              // "decrypt" or "unwrapKey" for private key imports
+ );
+  t.true(publicKey);
+  t.end();
+});
+
+test("RSA-OAEP - exportKey", async (t) => {
+  const key = await crypto.subtle.generateKey(
+    ({
+      name: "RSA-OAEP",
+      modulusLength: 2048, // can be 1024, 2048, or 4096
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: {name: "SHA-256"}, // can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+    } as Algorithm),
+    true, //  whether the key is extractable (i.e. can be used in exportKey)
+    ["encrypt", "decrypt"] //  can be any combination of "sign" and "verify"
+  );
+  const keydata = await crypto.subtle.exportKey(
+    "jwk", // can be "jwk" (public or private), "spki" (public only), or "pkcs8" (private only)
+    key.publicKey // can be a publicKey or privateKey, as long as extractable was true
+  );
+  t.true(keydata);
+  t.end();
+});
+
+test("RSA-OAEP - encrypt", async (t) => {
+  const key = await crypto.subtle.generateKey(
+    ({
+      name: "RSA-OAEP",
+      modulusLength: 2048, // can be 1024, 2048, or 4096
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: {name: "SHA-256"}, // can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+    } as Algorithm),
+    true, //  whether the key is extractable (i.e. can be used in exportKey)
+    ["encrypt", "decrypt"] //  can be any combination of "sign" and "verify"
+  );
+  const encrypted = await crypto.subtle.encrypt(
+    "RSA-OAEP",
+    key.publicKey, // from generateKey or importKey above
+    new Uint8Array(16) // ArrayBuffer of data you want to sign
+  );
+  t.true(new Uint8Array(encrypted));
+  t.end();
+});
+
+test("RSA-OAEP - decrypt", async (t) => {
+  const key = await crypto.subtle.generateKey(
+    ({
+      name: "RSA-OAEP",
+      modulusLength: 2048, // can be 1024, 2048, or 4096
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+      hash: {name: "SHA-256"}, // can be "SHA-1", "SHA-256", "SHA-384", or "SHA-512"
+    } as Algorithm),
+    true, //  whether the key is extractable (i.e. can be used in exportKey)
+    ["encrypt", "decrypt"] //  can be any combination of "sign" and "verify"
+  );
+  const encrypted = await crypto.subtle.encrypt(
+    "RSA-OAEP",
+    key.publicKey, // from generateKey or importKey above
+    new Uint8Array(16) // ArrayBuffer of data you want to sign
+  );
+  const decrypted = await crypto.subtle.decrypt(
+    "RSA-OAEP",
+    key.privateKey, // from generateKey or importKey above
+    encrypted // ArrayBuffer of data you want to sign
+  );
+  t.true(new Uint8Array(decrypted));
+  t.end();
+});
 
 test("example from https://blog.engelke.com/2014/06/22/symmetric-cryptography-in-the-browser-part-1/ should work", async (t) => {
   const aesKey = await crypto.subtle.generateKey(
