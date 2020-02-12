20dcaa24eea023b1aa89160aaf7506b18ad4bb06
sirpy <hadarr@gmail.com>
2020-02-12 09:26:29 +0200

    Working version (#12)
    * Use builtin WebView
    
    * add: working version ios/android
    
    * add: debug option
    
    * add: prepare for npm
    
    * fix: this.props
    
    * delete: tgz file
    
    * 0.0.18
    
    * add: remove random values
    
    * add: restore original repo
    
    * fix: remove random values
    
    * 0.0.19
    
    Co-authored-by: Linus Unnebäck <linus@folkdatorn.se>

---
+++ index.js
@@ -0,0 +1,158 @@
+const React = require("react");
+const { StyleSheet, View, Platform } = require("react-native");
+const { WebView } = require("react-native-webview");
+const { MainWorker, webViewWorkerString } = require("webview-crypto");
+
+const styles = StyleSheet.create({
+  hide: {
+    display: "none",
+    position: "absolute",
+
+    width: 0,
+    height: 0,
+
+    flexGrow: 0,
+    flexShrink: 1
+  }
+});
+
+const internalLibrary = `
+(function () {
+  function postMessage (message) {
+    if (window.ReactNativeWebView.postMessage === undefined) {
+      setTimeout(postMessage, 200, message)
+    } else {
+      window.ReactNativeWebView.postMessage(message)
+    }
+  }
+  var wvw = new WebViewWorker(postMessage)
+  //for android
+  window.document.addEventListener('message', (e) => {wvw.onMainMessage(e.data);})
+  //for ios
+  window.addEventListener('message', (e) => {wvw.onMainMessage(e.data);})
+}())
+`;
+
+let resolveWorker;
+let workerPromise = new Promise(resolve => {
+  resolveWorker = resolve;
+});
+
+function sendToWorker(message) {
+  workerPromise.then(worker => worker.onWebViewMessage(message));
+}
+
+const subtle = {
+  fake: true,
+  decrypt(...args) {
+    return workerPromise.then(worker => worker.crypto.subtle.decrypt(...args));
+  },
+  deriveBits(...args) {
+    return workerPromise.then(worker =>
+      worker.crypto.subtle.deriveBits(...args)
+    );
+  },
+  deriveKey(...args) {
+    return workerPromise.then(worker =>
+      worker.crypto.subtle.deriveKey(...args)
+    );
+  },
+  digest(...args) {
+    return workerPromise.then(worker => worker.crypto.subtle.digest(...args));
+  },
+  encrypt(...args) {
+    return workerPromise.then(worker => worker.crypto.subtle.encrypt(...args));
+  },
+  exportKey(...args) {
+    return workerPromise.then(worker =>
+      worker.crypto.subtle.exportKey(...args)
+    );
+  },
+  generateKey(...args) {
+    return workerPromise.then(worker =>
+      worker.crypto.subtle.generateKey(...args)
+    );
+  },
+  importKey(...args) {
+    return workerPromise.then(worker =>
+      worker.crypto.subtle.importKey(...args)
+    );
+  },
+  sign(...args) {
+    return workerPromise.then(worker => worker.crypto.subtle.sign(...args));
+  },
+  unwrapKey(...args) {
+    return workerPromise.then(worker =>
+      worker.crypto.subtle.unwrapKey(...args)
+    );
+  },
+  verify(...args) {
+    return workerPromise.then(worker => worker.crypto.subtle.verify(...args));
+  },
+  wrapKey(...args) {
+    return workerPromise.then(worker => worker.crypto.subtle.wrapKey(...args));
+  }
+};
+
+class PolyfillCrypto extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.webViewRef = React.createRef();
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  componentDidMount() {
+    const webView = this.webViewRef.current;
+
+    resolveWorker(
+      new MainWorker(msg => {
+        webView.postMessage(msg);
+      }, this.props.debug)
+    );
+  }
+
+  render() {
+    // The uri 'about:blank' doesn't have access to crypto.subtle on android
+    const uri = "file:///android_asset/blank.html";
+
+    // Base64 dance is to work around https://github.com/facebook/react-native/issues/20365
+    const code = `((function () {${webViewWorkerString};${internalLibrary}})())`;
+    const source = Platform.select({
+      android: { source: { uri } },
+      ios: undefined
+    });
+    return (
+      <View style={styles.hide}>
+        <WebView
+          injectedJavaScript={code}
+          javaScriptEnabled={true}
+          onError={a =>
+            console.error(Object.keys(a), a.type, a.nativeEvent.description)
+          }
+          onMessage={ev => sendToWorker(ev.nativeEvent.data)}
+          {...source}
+          ref={this.webViewRef}
+          originWhitelist={["*"]}
+        />
+      </View>
+    );
+  }
+}
+
+if (typeof global.crypto !== "object") {
+  global.crypto = {};
+}
+
+//required for webview-crypto serializer fromObject
+global.crypto.fake = true;
+
+if (typeof global.crypto.subtle !== "object") {
+  global.crypto.subtle = subtle;
+}
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = PolyfillCrypto;
---
+++ readme.md
@@ -0,0 +1,69 @@
+# react-native-webview-crypto
+
+[![npm](https://img.shields.io/npm/v/react-native-webview-crypto?style=flat-square)](https://www.npmjs.com/package/react-native-webview-crypto)
+
+Working version based on /saulshanabrook/react-native-webview-crypto and PR https://github.com/saulshanabrook/react-native-webview-crypto/pull/9
+
+This brings `window.crypto.subtle` to your React Native application. It does this by communicating with a hidden WebView, which performs the actual computation.
+
+Refer to the [`webview-crypto`](https://github.com/saulshanabrook/webview-crypto) repo for most of the code and some caveats.
+
+## Installation
+
+```sh
+npm install --save react-native-webview-crypto
+react-native link
+```
+
+### Android
+
+create a file called blank.html in android/app/src/main/assets
+
+```
+<html/>
+```
+
+### known issues
+
+if you use react-native-crypto and get warnings about cyclic require make sure to import it before the bridge
+
+```
+import react-native-crypto
+import WebviewCrypto from 'react-native-webview-crypto'
+```
+
+### getRandomValues
+
+for complete compatability with webcrypto (window.crypto.getRandomValues) you can install react-native-crytpo or react-native-get-random-values
+
+## Usage
+
+Rendering the `PolyfillCrypto` will start up a WebView to transparently proxy all the crypto calls to.
+
+```javascript
+import React, { Component } from "react";
+import { View } from "react-native";
+
+import App from "./app";
+
+import PolyfillCrypto from "react-native-webview-crypto";
+
+class TopLevelComponent extends Component {
+  render() {
+    return (
+      <View>
+        <PolyfillCrypto />
+        <App />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent("WhateverName", () => TopLevelComponent);
+```
+
+Now, in any of your code, you can access `window.crypto.subtle`, just like you would in a browser.
+
+There is also an [example repo](https://github.com/saulshanabrook/react-native-webview-crypto-example) which runs some example crypto using this library.
+
+_This project was funded by [Burke Software and Consulting LLC](http://burkesoftware.com/) for [passit](http://passit.io/)._
